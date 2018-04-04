5f9b3c9a4eabb9d22aa80212dc8d173b54a48d2c
gress <451148156@qq.com>
2018-04-04 15:06:42 +0800

    start project

---
+++ build/webpack.config.js
@@ -0,0 +1,44 @@
+const resolve = require("path").resolve;
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+
+module.exports = {
+  entry: {
+    index: resolve(__dirname, "../src/index.js"),
+    // style: resolve(__dirname, "../src/base.css"),
+  },
+  mode: process.env.NODE_ENV || "development",
+  output: {
+    libraryTarget: "commonjs2",
+    path: resolve(__dirname, "../dist"),
+    filename: "[name].js",
+    // publicPath: p
+  },
+  resolve: {
+    extensions: [".js", ".json", ".vue", ".css"],
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: "babel-loader",
+      include: [resolve(__dirname, "../src")]
+    },
+    // {
+    //   test: /\.css$/,
+    //   use: [
+    //     {loader: "style-loader"},
+    //     {loader: "css-loader"},
+    //   ]
+    // }
+  ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([{
+      // from: "./package.json",
+      from: resolve(__dirname, "../package.json"),
+      to: resolve(__dirname, "../dist")
+    }, {
+      from: resolve(__dirname, "../src/base.css"),
+      to: resolve(__dirname, "../dist")
+    }])
+  ]
+}
